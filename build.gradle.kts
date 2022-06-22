eee7d39bbb2eb7a3df319e20b78c09d4f76039f8
Adrian-Philipp Leuenberger <adrian-philipp.leuenberger@ergon.ch>
2022-06-22 09:12:44 +0200

    Use nexus as repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 
 allprojects {
     repositories {
-        mavenCentral()
+        maven { setUrl("https://nexus.ergon.ch/repository/secure-public") }
+        maven { setUrl("https://nexus.ergon.ch/repository/intermediates") }
     }
 }
