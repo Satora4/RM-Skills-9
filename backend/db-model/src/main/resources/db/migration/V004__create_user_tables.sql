9773b0d8c82914c3d3e359451d0b9cf210ed5766
Niculin Steiner <niculin.steiner@ergon.ch>
2022-06-30 15:13:38 +0200

    updated naming and added TEMA_TO_GROUP table

--- backend/db-model/src/main/resources/db/migration/V004__create_user_tables.sql
+++ backend/db-model/src/main/resources/db/migration/V004__create_user_tables.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS GAME
     POINTS_TEAM1  INT  NOT NULL,
     POINTS_TEAM2  INT  NOT NULL,
 
-    fk_Team1      INT  NOT NULL,
-    fk_Team2      INT  NOT NULL,
-    foreign key (fk_Team1) references TEAM(TEAM_ID),
+    TEAM_ID       INT  NOT NULL,
+    TEAM_ID2      INT  NOT NULL,
+    foreign key (TEAM_ID) references TEAM (TEAM_ID),
 
-    foreign key (fk_Team2) references TEAM(TEAM_ID)
-    );
+    foreign key (TEAM_ID2) references TEAM (TEAM_ID)
+);
---
+++ backend/db-model/src/main/resources/db/migration/V006__create_user_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS TEAM_TO_GROUP
+(
+    TEAM_ID  INT NOT NULL,
+    GROUP_ID INT NOT NULL,
+    foreign key (TEAM_ID) references TEAM (TEAM_ID),
+    foreign key (GROUP_ID) references GROUPE (GROUP_ID)
+);
