8ae35e3506c049ffa983c7fa59b9ad8cd72f7c6a
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-07-01 14:48:50 +0200

    adding columns to GameTO and GameController

--- backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/to/GameTO.java
+++ backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/to/GameTO.java
@@ -1,9 +1,14 @@
 package ch.ergon.lernende.wmtippspiel.backend.to;
 
 public class GameTO {
+
     private int id;
+    private String gameTime;
+    private String gameLocation;
     private int pointsTeam1;
     private int pointsTeam2;
+    private int teamId1;
+    private int teamId2;
 
     public int getId() {
         return id;
@@ -11,6 +16,22 @@ public class GameTO {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getGameTime() {
+        return gameTime;
+    }
+
+    public void setGameTime(String gameTime) {
+        this.gameTime = gameTime;
+    }
+
+    public String getGameLocation() {
+        return gameLocation;
+    }
+
+    public void setGameLocation(String gameLocation) {
+        this.gameLocation = gameLocation;
     }
 
     public int getPointsTeam1() {
@@ -27,5 +48,21 @@ public class GameTO {
 
     public void setPointsTeam2(int pointsTeam2) {
         this.pointsTeam2 = pointsTeam2;
+    }
+
+    public int getTeamId1() {
+        return teamId1;
+    }
+
+    public void setTeamId1(int teamId1) {
+        this.teamId1 = teamId1;
+    }
+
+    public int getTeamId2() {
+        return teamId2;
+    }
+
+    public void setTeamId2(int teamId2) {
+        this.teamId2 = teamId2;
     }
 }
