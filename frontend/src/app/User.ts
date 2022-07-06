d7f09c37f85f607588cabbc2d34e0e80752a5afb
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-06 11:22:36 +0200

    implement user-list-component and service for users

---
+++ frontend/src/app/User.ts
@@ -0,0 +1,9 @@
+export interface User{
+  id: number | 1;
+  first_name: string | null;
+  last_name: string | null;
+  email: string | null;
+  points: number | null;
+  ranking: number | null;
+  administrator: boolean | true;
+}
