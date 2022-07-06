08e2a674b8987f6e7b011bbd85c10d5de977af47
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-06 11:22:36 +0200

    implement user-list-component and service for users

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {PlayingScheduleComponent} from "./playing-schedule/playing-schedule.component";
 import { HttpClientModule } from '@angular/common/http';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HeaderComponent,
     PlayingScheduleComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule,
