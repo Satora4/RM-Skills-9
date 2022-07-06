b32194f951a4747da6df823301232bbd8e1117d2
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-06 10:06:15 +0200

    implement service for games and gameplan-component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {PlayingScheduleComponent} from "./playing-schedule/playing-schedule.component";
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    PlayingScheduleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
