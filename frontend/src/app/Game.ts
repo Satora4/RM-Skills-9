92003db5024e3e9894c50fb640c4d18da59ba3ce
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-06 10:06:15 +0200

    implement service for games and gameplan-component

---
+++ frontend/src/app/Game.ts
@@ -0,0 +1,9 @@
+export interface Game{
+  id:number | 1;
+  gameTime: Date | null;
+  gameLocation: string | null;
+  pointsTeam1: number | null;
+  pointsTeam2: number | null;
+  teamCountry1: string | null;
+  teamCountry2: string | null;
+}
---
+++ frontend/src/app/games.ts
@@ -0,0 +1,5 @@
+import {Game} from "./Game";
+
+export interface Games{
+  gameArray: Game[];
+}
