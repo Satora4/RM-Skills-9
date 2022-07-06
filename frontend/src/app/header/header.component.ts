cde399e1bcfb2c986bbace4e1690514409ad475f
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-06 15:44:20 +0200

    review revised

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private r:Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
--- frontend/src/app/playing-schedule/playing-schedule.model.ts
+++ frontend/src/app/playing-schedule/playing-schedule.model.ts
@@ -1,9 +1,9 @@
 export interface PlayingScheduleModel {
-  id:number | 1;
-  gameTime: Date | null;
-  gameLocation: string | null;
-  pointsTeam1: number | null;
-  pointsTeam2: number | null;
-  teamCountry1: string | null;
-  teamCountry2: string | null;
+  id:number;
+  gameTime: Date;
+  gameLocation: string;
+  pointsTeam1: number;
+  pointsTeam2: number;
+  teamCountry1: string;
+  teamCountry2: string;
 }
