e0013c3fbbc7b116785bc46931c13fe1d38940fe
Niculin Steiner <niculin.steiner@ergon.ch>
2022-07-15 09:39:30 +0200

    rules component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {PlayingScheduleComponent} from "./playing-schedule/playing-schedule.component";
 import {UserListComponent} from "./user-list/user-list.component";
 import {GroupComponent} from "./group/group.component";
+import {RulesComponent} from "./rules/rules.component";
 
 const routes: Routes = [
 
   {path: 'playing-schedule', component: PlayingScheduleComponent},
   {path: 'user-list', component: UserListComponent},
   {path: 'groups', component: GroupComponent},
+  {path: 'rules', component: RulesComponent},
   {path: '', redirectTo: '/playing-schedule', pathMatch: 'full'}
 ];
 
