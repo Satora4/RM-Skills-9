f3a4980e702ac8d85e73ff0a8b81ab3ae947e5b2
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-07-15 15:05:08 +0200

    TRAINEE-494, removing space

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 
 @Component({
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wm-tippspiel-frontend';
-
-
-
-
 }
