348c4c608eaebaf20773a5f6fad4f8ee2275d9a2
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-07-20 10:18:22 +0200

    TRAINEE-497, Angular Material added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {PlayingScheduleComponent} from "./playing-schedule/playing-schedule.comp
 import {HttpClientModule} from '@angular/common/http';
 import {UserListComponent} from './user-list/user-list.component';
 import {GroupComponent} from './group/group.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import {GroupComponent} from './group/group.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
 
   ],
   providers: [],
