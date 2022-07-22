7946154bfef53955b277c53a52e3e10d45b51643
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-07-22 16:39:40 +0200

    TRAINEE-506, reformat app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {GameComponent} from './game/game.component';
-import {GroupComponent} from './group/group.component';
-import {HeaderComponent} from './header/header.component';
-import {RulesComponent} from './rules/rules.component';
-import {TeamComponent} from './team/team.component';
-import {UserComponent} from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { GroupComponent } from './group/group.component';
+import { HeaderComponent } from './header/header.component';
+import { RulesComponent } from './rules/rules.component';
+import { TeamComponent } from './team/team.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, GameComponent, UserComponent, GroupComponent, TeamComponent, RulesComponent],
@@ -21,5 +21,4 @@ import {UserComponent} from './user/user.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
