c1db15758a18f56739d43ef9fd2b0342cb13f43a
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-08-03 13:33:04 +0200

    TRAINEE-512, corrections tip.model.ts

--- frontend/src/app/tip/tip.model.ts
+++ frontend/src/app/tip/tip.model.ts
@@ -1,10 +1,7 @@
-import { Game } from '../game/game.model';
-import { User } from '../user/user.model';
-
 export interface Tip {
   id: number;
-  userId: User["id"];
+  userId: number;
   tipTeam1: number;
   tipTeam2: number;
-  gameId: Game["id"];
+  gameId: number;
 }
