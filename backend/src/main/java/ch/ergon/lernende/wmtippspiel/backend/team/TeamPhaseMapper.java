03c3c82a696627beeab45c9aeb6855dfbcc94574
Joel Vontobel <joel.vontobel@vontobelfamily.ch>
2022-08-16 16:26:52 +0200

    TRAINEE-520, improve TeamPhaseMapper

--- backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/team/TeamPhaseMapper.java
+++ backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/team/TeamPhaseMapper.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class TeamPhaseMapper {
     public TeamPhaseTO map(TeamPhase teamPhase) {
-        return TeamPhaseTO.valueOf(teamPhase.name());
+        return switch (teamPhase) {
+            case GROUP_PHASE -> TeamPhaseTO.GROUP_PHASE;
+            case ROUND_OF_16 -> TeamPhaseTO.ROUND_OF_16;
+            case QUARTER_FINAL -> TeamPhaseTO.QUARTER_FINAL;
+            case SEMI_FINAL -> TeamPhaseTO.SEMI_FINAL;
+            case FINAL -> TeamPhaseTO.FINAL;
+        };
     }
 }
