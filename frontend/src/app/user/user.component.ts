daf251ab20107077cfece19b6532a5c7393d611a
Niculin Steiner <niculin.steiner@ergon.ch>
2022-09-02 08:06:46 +0200

    UX-Updates

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
   private users: User[] = [];
   private tips: Tip[] = [];
   userDataSource = new MatTableDataSource();
-  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'points', 'ranking'];
+  displayedColumns: string[] = ['ranking', 'points', 'firstName', 'lastName'];
   @ViewChild(MatSort) sort = new MatSort();
 
   constructor(private UserService: UserService) {
