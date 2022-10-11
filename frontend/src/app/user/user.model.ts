1c94665f39edd90c351c3faeedea64f0a86d9a29
Niculin Steiner <niculin.steiner@ergon.ch>
2022-10-11 14:01:27 +0200

    TRAINEE-549: reformatted

--- frontend/src/app/user/user.model.ts
+++ frontend/src/app/user/user.model.ts
@@ -2,8 +2,8 @@ export interface User {
   id: number;
   firstName: string;
   lastName: string;
-  points: number;
   email:string;
+  points: number;
   ranking: number;
   administrator: boolean;
 }
