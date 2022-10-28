b9ab4e54f0f578e0013970f609984945560b3f73
jvontobe <joel.vontobel@ergon.ch>
2022-10-28 14:00:46 +0200

    TRAINEE-538: check in the BE if user is admin

--- backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/pointscalculator/CalculatorController.java
+++ backend/src/main/java/ch/ergon/lernende/wmtippspiel/backend/pointscalculator/CalculatorController.java
@@ -1,5 +1,7 @@
 package ch.ergon.lernende.wmtippspiel.backend.pointscalculator;
 
+import ch.ergon.lernende.wmtippspiel.backend.user.CurrentUser;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,14 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
+    private final CurrentUser currentUser;
 
-    public CalculatorController(CalculatorService calculatorService) {
+    public CalculatorController(CalculatorService calculatorService, CurrentUser currentUser) {
         this.calculatorService = calculatorService;
+        this.currentUser = currentUser;
     }
 
     @PostMapping("/calculate")
-    public void calculate() {
-        calculatorService.calculateScore();
-        calculatorService.calculateGames();
+    public HttpStatus calculate() {
+        if (currentUser.getUser().isAdministrator()) {
+            calculatorService.calculateScore();
+            calculatorService.calculateGames();
+            return HttpStatus.OK;
+        } else {
+            return HttpStatus.BAD_REQUEST;
+        }
     }
 }
